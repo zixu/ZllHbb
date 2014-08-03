023d0039744812c46405e6c854ac2237e46e5a3f
zixu <zijun.xu@cern.ch>
2014-08-02 19:01:26 -0500

    corrected xs-reweight for MC, corrected btag

--- Draw_ControlPlots/runStep2Analyzer.C
+++ Draw_ControlPlots/runStep2Analyzer.C
@@ -18,41 +18,48 @@ void runStep2Analyzer() {
     setTDRStyle(); //plotting style
 
     
-	TString path="../histos/";
+	TString path="./histos/";
 	Double_t lumi=19624;
 
 
-/*
+
 	//signal
-	runStep2Analyzer_signle(1,             lumi, path, "Step2_ZHiggs0P_M-125p6_8TeV-JHUGenV4-private_local");
+	runStep2Analyzer_signle(1,             lumi,   100000, path, "Step2_ZHiggs0P_M-125p6_8TeV-JHUGenV4-private");
 
 	//backgrounds
-//	runStep2Analyzer_signle(234,           lumi, path, "DiJetPt_TTJets_Merged");
-	runStep2Analyzer_signle(234,           lumi, path, "DiJetPt_TT_8TeV-mcatnlo");
-	runStep2Analyzer_signle(2950.0 * 1.188,lumi, path, "DiJetPt_DYJetsToLL_M-50_TuneZ2Star_8TeV-madgraph-tarball");
-//	runStep2Analyzer_signle(1.,            lumi, path, "DiJetPt_DYJetsToLL_PtZ-50To70_TuneZ2star_8TeV-madgraph-tarball");
-//	runStep2Analyzer_signle(1.,            lumi, path, "DiJetPt_DYJetsToLL_PtZ-70To100_TuneZ2star_8TeV-madgraph-tarball");
-//	runStep2Analyzer_signle(1.,            lumi, path, "DiJetPt_DYJetsToLL_PtZ-100_TuneZ2star_8TeV-madgraph");
-//	runStep2Analyzer_signle(1.,            lumi, path, "DiJetPt_DY1JetsToLL_M-50_TuneZ2Star_8TeV-madgraph");
-//	runStep2Analyzer_signle(1.,            lumi, path, "DiJetPt_DY2JetsToLL_M-50_TuneZ2Star_8TeV-madgraph");
-//	runStep2Analyzer_signle(1.,            lumi, path, "DiJetPt_DY4JetsToLL_M-50_TuneZ2Star_8TeV-madgraph");
-	runStep2Analyzer_signle(5.9,           lumi, path, "DiJetPt_ZZ_TuneZ2star_8TeV_pythia6_tauola");
-	runStep2Analyzer_signle(18.3,          lumi, path, "DiJetPt_WZ_TuneZ2star_8TeV_pythia6_tauola");
-	runStep2Analyzer_signle(43,            lumi, path, "DiJetPt_WW_TuneZ2star_8TeV_pythia6_tauola");
-	runStep2Analyzer_signle(3.19,          lumi, path, "DiJetPt_T_s-channel_TuneZ2star_8TeV-powheg-tauola");
-	runStep2Analyzer_signle(41.92,         lumi, path, "DiJetPt_T_t-channel_TuneZ2star_8TeV-powheg-tauola");
-	runStep2Analyzer_signle(7.87,          lumi, path, "DiJetPt_T_tW-channel-DR_TuneZ2star_8TeV-powheg-tauola");
-	runStep2Analyzer_signle(1.44,          lumi, path, "DiJetPt_Tbar_s-channel_TuneZ2star_8TeV-powheg-tauola");
-	runStep2Analyzer_signle(22.65,         lumi, path, "DiJetPt_Tbar_t-channel_TuneZ2star_8TeV-powheg-tauola");
-	runStep2Analyzer_signle(7.87,          lumi, path, "DiJetPt_Tbar_tW-channel-DR_TuneZ2star_8TeV-powheg-tauola");
-*/
+//	runStep2Analyzer_signle(234,           lumi,        ,  path, "DiJetPt_TTJets_Merged");
+	runStep2Analyzer_signle(234,           lumi, 27519328.0000,  path, "DiJetPt_TT_8TeV-mcatnlo");
+	runStep2Analyzer_signle(2950.0 * 1.188,lumi, 28640004.0000,  path, "DiJetPt_DYJetsToLL_M-50_TuneZ2Star_8TeV-madgraph-tarball");
+//	runStep2Analyzer_signle(1.,            lumi,         ,  path, "DiJetPt_DYJetsToLL_PtZ-50To70_TuneZ2star_8TeV-madgraph-tarball");
+//	runStep2Analyzer_signle(1.,            lumi,         ,  path, "DiJetPt_DYJetsToLL_PtZ-70To100_TuneZ2star_8TeV-madgraph-tarball");
+//	runStep2Analyzer_signle(1.,            lumi,         ,  path, "DiJetPt_DYJetsToLL_PtZ-100_TuneZ2star_8TeV-madgraph");
+//	runStep2Analyzer_signle(1.,            lumi,         ,  path, "DiJetPt_DY1JetsToLL_M-50_TuneZ2Star_8TeV-madgraph");
+//	runStep2Analyzer_signle(1.,            lumi,         ,  path, "DiJetPt_DY2JetsToLL_M-50_TuneZ2Star_8TeV-madgraph");
+//	runStep2Analyzer_signle(1.,            lumi,         ,  path, "DiJetPt_DY4JetsToLL_M-50_TuneZ2Star_8TeV-madgraph");
+	runStep2Analyzer_signle(5.9,           lumi,  9809065.0000,  path, "DiJetPt_ZZ_TuneZ2star_8TeV_pythia6_tauola");
+	runStep2Analyzer_signle(18.3,          lumi, 10008316.0000,  path, "DiJetPt_WZ_TuneZ2star_8TeV_pythia6_tauola");
+	runStep2Analyzer_signle(43,            lumi, 10007584.0000,  path, "DiJetPt_WW_TuneZ2star_8TeV_pythia6_tauola");
+	runStep2Analyzer_signle(3.19,          lumi,   259923.5000,  path, "DiJetPt_T_s-channel_TuneZ2star_8TeV-powheg-tauola");
+	runStep2Analyzer_signle(41.92,         lumi,  3144815.0000,  path, "DiJetPt_T_t-channel_TuneZ2star_8TeV-powheg-tauola");
+	runStep2Analyzer_signle(7.87,          lumi,   498080.5938,  path, "DiJetPt_T_tW-channel-DR_TuneZ2star_8TeV-powheg-tauola");
+	runStep2Analyzer_signle(1.44,          lumi,   140163.6406,  path, "DiJetPt_Tbar_s-channel_TuneZ2star_8TeV-powheg-tauola");
+	runStep2Analyzer_signle(22.65,         lumi,  1933425.6250,  path, "DiJetPt_Tbar_t-channel_TuneZ2star_8TeV-powheg-tauola");
+	runStep2Analyzer_signle(7.87,          lumi,   493595.9062,  path, "DiJetPt_Tbar_tW-channel-DR_TuneZ2star_8TeV-powheg-tauola");
 	//data
-	runStep2Analyzer_signle(1,             lumi, path, "DiJetPt_SingleMuRun2012", 1);
+	//runStep2Analyzer_signle(1,             lumi,       -1,  path, "DiJetPt_SingleMuRun2012", 1);
+	runStep2Analyzer_signle(1,             lumi,       -1,  path, "DiJetPt_SingleMuRun2012AAug06EdmV42", 1);
+	runStep2Analyzer_signle(1,             lumi,       -1,  path, "DiJetPt_SingleMuRun2012AJul13EdmV42", 1);
+	runStep2Analyzer_signle(1,             lumi,       -1,  path, "DiJetPt_SingleMuRun2012BJul13EdmV42", 1);
+	runStep2Analyzer_signle(1,             lumi,       -1,  path, "DiJetPt_SingleMuRun2012C-EcalRecover_11Dec2012-v1_v2", 1);
+	runStep2Analyzer_signle(1,             lumi,       -1,  path, "DiJetPt_SingleMuRun2012CAug24RerecoEdmV42", 1);
+	runStep2Analyzer_signle(1,             lumi,       -1,  path, "DiJetPt_SingleMuRun2012CPromptV2TopUpEdmV42", 1);
+	runStep2Analyzer_signle(1,             lumi,       -1,  path, "DiJetPt_SingleMuRun2012CPromptv2EdmV42", 1);
+	runStep2Analyzer_signle(1,             lumi,       -1,  path, "DiJetPt_SingleMuRun2012D-PromptReco-v1", 1);
 
 
 }
 
-void runStep2Analyzer_signle( Double_t xs, Double_t lumi,TString path, TString samplename, bool isData=0) 
+void runStep2Analyzer_signle( Double_t xs, Double_t lumi, Double_t num_entrise, TString path, TString samplename, bool isData=0) 
 {
 
 	// mkdir plots fold accoding time
@@ -72,7 +79,7 @@ void runStep2Analyzer_signle( Double_t xs, Double_t lumi,TString path, TString s
 	if(tree1){
 
 		Step2Analyzer* analyzor = new Step2Analyzer(tree1, plot_Dir_DateTime);
-		analyzor->Loop(samplename, xs, lumi, isData );
+		analyzor->Loop(samplename, xs, lumi, num_entrise, isData );
 
 	}else{
 		cout<<"Error! Coundn't find a tree!!!"<<endl;
